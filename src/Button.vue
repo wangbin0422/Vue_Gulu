ca97b50aa04782cc6296bf9f0836740fcd401687
foreverwb <504543582@qq.com>
2019-05-02 16:04:34 +0800

    vue init

---
+++ src/Button.vue
@@ -0,0 +1,15 @@
+<template>
+  <button class="ui-btn">按钮</button>
+</template>
+
+<script>
+  export default {
+    name: 'Button'
+  };
+</script>
+
+<style lang="scss">
+.ui-btn {
+  padding: 0 1em;
+}
+</style>
---
+++ src/app.js
@@ -0,0 +1,8 @@
+import Vue from 'vue';
+import Button from './Button';
+
+Vue.component('ui-button', Button);
+
+new Vue({
+  el: '#app',
+});
