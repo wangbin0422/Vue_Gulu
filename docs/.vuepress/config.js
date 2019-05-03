ee678a0ccf98789e8eb74f2cba9904dd2e0c487e
foreverwb <504543582@qq.com>
2019-05-03 21:40:16 +0800

    vuepress

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+module.exports = {
+  base: '/Vue_Gulu/',
+  title: 'UI_Gulu',
+  description: 'UI框架',
+  themeConfig: {
+    nav: [
+      {text: '主页', link: '/'},
+      {text: '文档', link: '/guide/'},
+      {text: '交流', link: 'https://google.com'},
+    ],
+    sidebar: [
+      {
+        title: '入门',
+        collapsable: false,
+        children: [
+          '/install/',
+          '/get-started/',
+        ]
+      },
+      {
+        title: '组件',
+        collapsable: false,
+        children: [
+          '/components/button',
+        ]
+      },
+
+    ]
+  },
+  scss: {
+    includePaths: [path.join(__dirname, '../../styles')]
+  }
+};
--- src/app.js
+++ src/app.js
@@ -1,9 +1,7 @@
 import Vue from 'vue';
 import Button from './Button/Button';
-import Icon from './Icon';
 
 Vue.component('ui-button', Button);
-Vue.component('ui-icon', Icon);
 
 new Vue({
   el: '#app',
